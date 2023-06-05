12e43d60a402453a48e615df15a0dd9c235d19c4
dittmptr27 <adityamuhamadputra@gmail.com>
2023-06-05 15:43:14 +0700

    remove unused code

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:get/get.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
